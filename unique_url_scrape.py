a7cefa2665acdf7347ed62c0119ca93df1d3618d
Peter Berzins <peter.m.berzins@gmail.com>
2018-06-15 13:49:09 -0700

    Cleaned up formatting and docstrings

--- unique_url_scrape.py
+++ unique_url_scrape.py
@@ -1,42 +1,31 @@
-
-# coding: utf-8
-
-# In[103]:
-
-
 import requests
 import pymongo
 from bs4 import BeautifulSoup
 
 
-# In[104]:
 
 
 """
-ASSIGNMENT FOR WEB SCRAPING
-Write a function that scrapes a URL using `requests` 
+Write a function that scrapes a URL using `requests`
 Stores the response content in a database.
 If you have already scraped the URL,
-return the stored content instead of scraping it again."""
+return the stored content instead of scraping it again.
+"""
 
 
-# In[106]:
-
-
+#Start a Mongo Database 'Scraped', with collection 'Website'
 mc = pymongo.MongoClient('mongodb://localhost:27017/')
 scrape_db = mc['scraped']
 sites_collection = scrape_db['websites']
 
 
-# In[107]:
-
 
 def scrape_site(url: str):
     """ Takes in a url as a string
-        If the url is already in the data base, 
+        If the url is already in the data base,
         Return the content of the URL
-        If it is not in the data base, 
-        add it with 'url' : url... and 'data' : html data....
+        If it is not in the data base,
+        add it with {'url' : url... and 'data' : html data...}
         and Return the content
     """
     data1 = checker(url)
@@ -50,10 +39,6 @@ def scrape_site(url: str):
         durka['data'] = data
         sites_collection.insert_one(durka)
         return data
-   
-
-
-# In[108]:
 
 
 def get_data_from_database(url):
@@ -63,8 +48,6 @@ def get_data_from_database(url):
     """
     cursor = sites_collection.find()
     for site in cursor:
-        if site['url'] == url: 
+        if site['url'] == url:
             return site['data']
-     
-    return None 
-
+    return None
